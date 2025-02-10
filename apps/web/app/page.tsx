bd5d6f0e2ea17cf327e1565fbcc91c2f40d3cbf9
Robert Aron <robert.aron.tx@gmail.com>
2025-02-10 12:02:10 -0600

    remove layout and see if it works

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,15 @@
 import Link from "next/link";
 
+
 export default async function IndexPage() {
   return (
     <div>
       <h1>Hello World</h1>
       <div>
-        <Link href="/some-layout">other</Link>
+      <Link href='/some-layout'>other</Link>
       </div>
       <div>
-        <Link href="/broken">broken</Link>
+      <Link href='/broken'>broken</Link>
       </div>
     </div>
   );
