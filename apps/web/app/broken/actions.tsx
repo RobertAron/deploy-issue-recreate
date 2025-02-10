df13eec240757fee3a45af4e157e42a5ff4c6865
Robert Aron <robert.aron.tx@gmail.com>
2025-02-10 14:52:19 -0600

    wtff

--- apps/web/app/broken/actions.tsx
+++ apps/web/app/broken/actions.tsx
@@ -1,15 +1,16 @@
 "use server";
 
-console.log('pre import')
+console.log("pre import");
 import { prisma } from "../../other/getPrismaClient";
-console.log('post import')
+console.log("post import");
 
 export async function getData() {
-  console.log('did I even here here?')
+  console.log("did I even here here?");
   try {
     const data = await prisma.form_response.findFirst();
     return data;
   } catch (err) {
+    console.log("in the error section");
     console.log(err);
     return { data: "got an error...", err };
   }
