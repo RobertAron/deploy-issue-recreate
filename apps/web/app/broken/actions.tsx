2eb7abfdc4b5f14f92d295fcec099d3ae420d03a
Robert Aron <robert.aron.tx@gmail.com>
2025-02-10 14:46:03 -0600

    error thingy

--- apps/web/app/broken/actions.tsx
+++ apps/web/app/broken/actions.tsx
@@ -3,11 +3,11 @@
 import { prisma } from "../../other/getPrismaClient";
 
 export async function getData() {
-  try{
+  try {
     const data = await prisma.form_response.findFirst();
-    return data
-  }
-  catch(err){
-    return err
+    return data;
+  } catch (err) {
+    console.log(err);
+    return { data: "got an error...", err };
   }
 }
